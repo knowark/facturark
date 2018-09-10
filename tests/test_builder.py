6d0634b97547b91ebd29af0e4c60c529966507e1
Esteban Echeverry <eecheverry@nubark.com>
2018-09-10 16:44:41 -0500

    Definir método build del Builder

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,7 +1,19 @@
+from pytest import fixture
+from facturark.models import Invoice
 from facturark.builder import Builder
 
 
-def test_builder_creation():
+@fixture
+def builder():
     builder = Builder()
+    return builder
 
+
+def test_builder_creation(builder):
     assert builder
+
+
+def test_build(builder):
+    invoice = Invoice()
+    result = builder.build(Invoice)
+    assert result
