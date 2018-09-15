f6cf2cbbddd320d407e70c56c501adfcdf832d7e
Esteban Echeverry <eecheverry@nubark.com>
2018-09-14 19:15:01 -0500

    Usar randint para generar el nonce

--- facturark/client.py
+++ facturark/client.py
@@ -1,5 +1,5 @@
 import zeep
-from random import random
+from random import randint
 from base64 import b64encode
 from datetime import datetime
 from lxml.etree import tostring
@@ -9,7 +9,7 @@ from zeep.wsse.username import UsernameToken
 class Client:
 
     def __init__(self, username, password, wsdl_url):
-        nonce = b64encode(bytes(random()))
+        nonce = b64encode(bytes(randint(0, 100000000)))
         created = datetime.now().isoformat(sep='T')
 
         self.client = zeep.Client(
