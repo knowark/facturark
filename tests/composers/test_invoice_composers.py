2ae215169aa21baae942b3a4ed22d81e82264fdb
Esteban Echeverry <eecheverry@nubark.com>
2018-09-17 17:54:23 -0500

    Crear campos básicos de la factura del namespace cbc

--- tests/composers/test_invoice_composers.py
+++ tests/composers/test_invoice_composers.py
@@ -8,27 +8,33 @@ from facturark.composers import NS
 
 
 @fixture
-def serializer():
+def composer():
     return InvoiceComposer()
 
 
-class TestInvoiceSerializer(XmlTestCase):
+class TestInvoiceComposer(XmlTestCase):
 
     @fixture(autouse=True)
-    def inject_fixtures(self, invoice_dict, serializer):
+    def inject_fixtures(self, invoice_dict, composer):
         self.invoice_dict = invoice_dict
-        self.serializer = serializer
+        self.composer = composer
 
     def test_invoice_compose(self):
-        invoice = self.serializer.compose(self.invoice_dict)
+        invoice = self.composer.compose(self.invoice_dict)
 
         assert invoice.prefix == "fe"
         assert invoice.tag == QName(NS.fe, "Invoice").text
         assert invoice.findtext(QName(NS.cbc, "UBLVersionID")) == "UBL 2.0"
         assert invoice.findtext(QName(NS.cbc, "ProfileID")) == "DIAN 1.0"
         assert invoice.findtext(QName(NS.cbc, "ID")) == "F0001"
+        assert invoice.findtext(QName(NS.cbc, "UUID")) == (
+            "a3d6c86a71cbc066aaa19fd363c0fe4b5778d4a0")
+        assert invoice.findtext(QName(NS.cbc, "IssueDate")) == "2018-09-13"
+        assert invoice.findtext(QName(NS.cbc, "IssueTime")) == "00:31:40"
+        assert invoice.findtext(QName(NS.cbc, "InvoiceTypeCode")) == "1"
+        assert invoice.findtext(QName(NS.cbc, "DocumentCurrencyCode")) == "COP"
 
     def test_invoice_serialize(self):
-        document = self.serializer.serialize(self.invoice_dict)
+        document = self.composer.serialize(self.invoice_dict)
 
         self.assertXmlDocument(document)
