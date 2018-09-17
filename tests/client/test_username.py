2676a97a3540275a7c5f37a203c7fbbe9bb2dde1
Esteban Echeverry <eecheverry@nubark.com>
2018-09-16 22:41:55 -0500

    Completar cobertura de los archivos de transporte y username

---
+++ tests/client/test_username.py
@@ -0,0 +1,22 @@
+from lxml.etree import fromstring, tostring
+from facturark.client.username import UsernameToken
+
+
+def test_username_token_apply():
+    envelope = fromstring('<Envelope></Envelope>')
+    headers = {}
+
+    username = UsernameToken("USER", "PASS")
+    envelope, headers = username.apply(envelope, headers)
+
+    assert envelope is not None
+    assert 'USER'in tostring(envelope)
+
+
+def test_username_token_verify():
+    envelope = fromstring('<Envelope></Envelope>')
+    username = UsernameToken("USER", "PASS")
+
+    result = username.verify(envelope)
+
+    assert result is None
