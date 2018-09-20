d96cfd6eeef6b28c6190441dd70fb55d07c88afb
Esteban Echeverry <eecheverry@nubark.com>
2018-09-20 16:16:13 -0500

    Crear Person composer

--- facturark/composers/__init__.py
+++ facturark/composers/__init__.py
@@ -4,4 +4,5 @@ from .invoice_composer import InvoiceComposer
 from .item_composer import ItemComposer
 from .party_composer import PartyComposer
 from .party_tax_scheme_composer import PartyTaxSchemeComposer
+from .person_composer import PersonComposer
 from .price_composer import PriceComposer
---
+++ tests/composers/test_person_composer.py
@@ -0,0 +1,30 @@
+from pytest import fixture
+from lxml.etree import QName, fromstring
+from facturark.composers import NS, PersonComposer
+
+
+@fixture
+def composer():
+    return PersonComposer()
+
+
+@fixture
+def data_dict():
+    return {
+        'first_name': "Gabriel",
+        'family_name': "Echeverry"
+    }
+
+
+def test_compose(composer, data_dict, schema):
+    person = composer.compose(data_dict)
+
+    assert person.tag == QName(NS.fe, "Person").text
+    assert person.findtext(QName(NS.cbc, "FirstName")) == "Gabriel"
+    assert person.findtext(QName(NS.cbc, "FamilyName")) == "Echeverry"
+    schema.assertValid(person)
+
+
+def test_serialize(composer, data_dict):
+    document = composer.serialize(data_dict)
+    assert fromstring(document) is not None
