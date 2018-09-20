ae94a1220424ffe2c1c52442e7986d3af3237c6f
Esteban Echeverry <eecheverry@nubark.com>
2018-09-20 15:42:15 -0500

    Crear PartyTaxScheme Composer

--- facturark/composers/__init__.py
+++ facturark/composers/__init__.py
@@ -3,4 +3,5 @@ from .address_composer import AddressComposer
 from .invoice_composer import InvoiceComposer
 from .item_composer import ItemComposer
 from .party_composer import PartyComposer
+from .party_tax_scheme_composer import PartyTaxSchemeComposer
 from .price_composer import PriceComposer
---
+++ tests/composers/test_party_tax_scheme_composer.py
@@ -0,0 +1,29 @@
+from pytest import fixture
+from lxml.etree import QName, fromstring
+from facturark.composers import NS, PartyTaxSchemeComposer
+
+
+@fixture
+def composer():
+    return PartyTaxSchemeComposer()
+
+
+@fixture
+def data_dict():
+    return {
+        'tax_level_code': '0'
+    }
+
+
+def test_compose(composer, data_dict, schema):
+    party_tax_scheme = composer.compose(data_dict)
+
+    assert party_tax_scheme.tag == QName(NS.fe, "PartyTaxScheme").text
+    assert party_tax_scheme.findtext(QName(NS.cbc, "TaxLevelCode")) == "0"
+    assert party_tax_scheme.find(QName(NS.cac, "TaxScheme")) is not None
+    schema.assertValid(party_tax_scheme)
+
+
+def test_serialize(composer, data_dict):
+    document = composer.serialize(data_dict)
+    assert fromstring(document) is not None
