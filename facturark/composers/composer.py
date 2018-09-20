6f5a1ccd07dc51e2d085605f6f6e8190180a506c
Esteban Echeverry <eecheverry@nubark.com>
2018-09-20 16:41:36 -0500

    Crear clase base para los composers para definir funcionalidad cómun y estándar

---
+++ facturark/composers/composer.py
@@ -0,0 +1,24 @@
+from lxml.etree import Element, SubElement, QName, tostring
+from .namespaces import NS
+from .utils import make_child
+
+
+class Composer:
+
+    def compose(self, data_dict, root_name=None):
+        raise NotImplementedError("The 'compose' method should be "
+                                  "implemented by subclasses.")
+
+    def serialize(self, data_dict, root_name=None):
+        root_name = root_name or self.root_name
+        root = self.compose(data_dict, root_name)
+        document = tostring(root,
+                            method='xml',
+                            encoding='utf-8',
+                            pretty_print=True,
+                            xml_declaration=True)
+        return document
+
+    @property
+    def root_name(self):
+        return self.__class__.__name__.replace('Composer', '')
