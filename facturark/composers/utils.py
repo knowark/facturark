679bb23b42e5c5119a61280fe621b9326ec9291e
Esteban Echeverry <eecheverry@nubark.com>
2018-09-20 11:48:36 -0500

    Remover comentarios de utils

--- facturark/composers/utils.py
+++ facturark/composers/utils.py
@@ -1,18 +1,6 @@
 from lxml.etree import SubElement
 
 
-# def make_child(parent, tag, text=None, attributes=None, required=True):
-#     if required and not (text or attributes):
-#         raise ValueError("The <{}> tag is required".format(tag))
-
-#     child = SubElement(parent, tag, attributes)
-
-#     if text:
-#         child.text = text
-
-#     return child
-
-
 def make_child(parent, tag, text=None, attributes=None,
                required=True, empty=False):
     if empty and (text or attributes):
