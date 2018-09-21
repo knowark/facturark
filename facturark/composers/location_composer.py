a6a85d1d6a645f72ca95b22f5aa9caa44d40b7ab
Esteban Echeverry <eecheverry@nubark.com>
2018-09-20 23:05:33 -0500

    Crear LocationComposer. WIP

---
+++ facturark/composers/location_composer.py
@@ -0,0 +1,20 @@
+from lxml.etree import Element, QName
+from .namespaces import NS
+from .composer import Composer
+from .utils import make_child
+
+
+class LocationComposer(Composer):
+
+    def __init__(self, address_composer):
+        self.address_composer = address_composer
+
+    def compose(self, data_dict, root_name=None):
+        root_name = root_name or self.root_name
+        root = Element(QName(NS.fe, root_name), nsmap=vars(NS))
+
+        address_dict = data_dict.get('address')
+        if address_dict:
+            self.address_composer.compose(address_dict)
+
+        return root
