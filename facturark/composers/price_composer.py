042a182dfec9441c774cd8a42e77fb80ec680ddf
Esteban Echeverry <eecheverry@nubark.com>
2018-09-20 22:37:28 -0500

    Usar make_child en PriceComposer

--- facturark/composers/price_composer.py
+++ facturark/composers/price_composer.py
@@ -1,6 +1,7 @@
-from lxml.etree import Element, SubElement, QName, tostring
+from lxml.etree import Element, QName
 from .namespaces import NS
 from .composer import Composer
+from .utils import make_child
 
 
 class PriceComposer(Composer):
@@ -9,9 +10,9 @@ class PriceComposer(Composer):
         root_name = root_name or self.root_name
         root = Element(QName(NS.fe, root_name), nsmap=vars(NS))
 
-        SubElement(root, QName(
-            NS.cbc, "PriceAmount"),
-            currencyID=data_dict['price_amount']['@currency_id']).text = str(
-                float(data_dict['price_amount']['#text']))
+        price_amount_dict = data_dict['price_amount']
+        make_child(root, QName(NS.cbc, "PriceAmount"),
+                   str(float(price_amount_dict['#text'])),
+                   {'currencyID': price_amount_dict['@currency_id']})
 
         return root
