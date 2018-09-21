f98f82d7476df8a4ed1a86e11db9e48adc486302
Esteban Echeverry <eecheverry@nubark.com>
2018-09-20 22:49:06 -0500

    Crear PaymentComposer

--- facturark/composers/__init__.py
+++ facturark/composers/__init__.py
@@ -8,6 +8,7 @@ from .item_composer import ItemComposer
 from .monetary_total_composer import MonetaryTotalComposer
 from .party_composer import PartyComposer
 from .party_tax_scheme_composer import PartyTaxSchemeComposer
+from .payment_composer import PaymentComposer
 from .person_composer import PersonComposer
 from .price_composer import PriceComposer
 from .tax_subtotal_composer import TaxSubtotalComposer
