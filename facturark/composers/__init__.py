f631af6e93519da7eebb9b0d9e773cea487a64e2
Esteban Echeverry <eecheverry@nubark.com>
2018-09-20 19:13:37 -0500

    Crear TaxSubtotalComposer

--- facturark/composers/__init__.py
+++ facturark/composers/__init__.py
@@ -7,3 +7,4 @@ from .party_composer import PartyComposer
 from .party_tax_scheme_composer import PartyTaxSchemeComposer
 from .person_composer import PersonComposer
 from .price_composer import PriceComposer
+from .tax_subtotal_composer import TaxSubtotalComposer
---
+++ tests/composers/test_tax_subtotal_composer.py
@@ -0,0 +1,53 @@
+from pytest import fixture
+from lxml.etree import QName, fromstring
+from facturark.composers import NS
+from facturark.composers import TaxSubtotalComposer
+
+
+@fixture
+def composer():
+    return TaxSubtotalComposer()
+
+
+@fixture
+def data_dict():
+    return {
+        'percent': 19,
+        'taxable_amount': {
+            '@currency_id': 'COP',
+            '#text': 11345892
+        },
+        'tax_amount': {
+            '@currency_id': 'COP',
+            '#text': 2155719.48
+        },
+        'tax_category': {
+            'tax_scheme': {
+                'id': '03'
+            }
+        }
+    }
+
+
+def test_compose(composer, data_dict, schema):
+    tax_subtotal = composer.compose(data_dict)
+
+    assert tax_subtotal.tag == QName(NS.fe, "TaxSubtotal").text
+
+    taxable_amount = tax_subtotal.find(
+        QName(NS.cbc, "TaxableAmount"))
+    assert float(taxable_amount.text) == 11345892
+    assert taxable_amount.attrib['currencyID'] == 'COP'
+
+    tax_amount = tax_subtotal.find(
+        QName(NS.cbc, "TaxAmount"))
+    assert float(tax_amount.text) == 2155719.48
+    assert tax_amount.attrib['currencyID'] == 'COP'
+
+    assert float(tax_subtotal.findtext(QName(NS.cbc, "Percent"))) == 19
+
+    tax_category = tax_subtotal.find(QName(NS.cac, "TaxCategory"))
+    tax_scheme = tax_category.find(QName(NS.cac, "TaxScheme"))
+    assert tax_scheme.findtext(QName(NS.cbc, "ID")) == '03'
+
+    schema.assertValid(tax_subtotal)
