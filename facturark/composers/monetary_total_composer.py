adb0231962f065a186954177a26889b0394f4ac4
Esteban Echeverry <eecheverry@nubark.com>
2018-09-23 12:28:16 -0500

    Reemplazar el uso de nombres de atributo por el diccionario de atributos en el monetary composer

--- facturark/composers/monetary_total_composer.py
+++ facturark/composers/monetary_total_composer.py
@@ -6,26 +6,21 @@ from .utils import make_child
 
 class MonetaryTotalComposer(Composer):
 
+    def __init__(self, amount_composer):
+        self.amount_composer = amount_composer
+
     def compose(self, data_dict, root_name=None):
         root_name = root_name or self.root_name
         root = Element(QName(NS.fe, root_name), nsmap=vars(NS))
 
-        line_extension_amount = data_dict['line_extension_amount']
-        make_child(root, QName(NS.cbc, 'LineExtensionAmount'),
-                   str(float(line_extension_amount['#text'])),
-                   {'currencyID': line_extension_amount['@currency_id']},
-                   required=True)
+        line_extension_amount = self.amount_composer.compose(
+            data_dict['line_extension_amount'], 'LineExtensionAmount')
+        root.append(line_extension_amount)
 
-        tax_exclusive_amount = data_dict['tax_exclusive_amount']
-        make_child(root, QName(NS.cbc, 'TaxExclusiveAmount'),
-                   str(float(tax_exclusive_amount['#text'])),
-                   {'currencyID': tax_exclusive_amount['@currency_id']},
-                   required=True)
+        root.append(self.amount_composer.compose(
+            data_dict['tax_exclusive_amount'], 'TaxExclusiveAmount'))
 
-        payable_amount = data_dict['payable_amount']
-        make_child(root, QName(NS.cbc, 'PayableAmount'),
-                   str(float(payable_amount['#text'])),
-                   {'currencyID': payable_amount['@currency_id']},
-                   required=True)
+        root.append(self.amount_composer.compose(
+            data_dict['payable_amount'], 'PayableAmount'))
 
         return root
