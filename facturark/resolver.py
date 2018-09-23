f4160251e3f5bc20e983af80eacdc8b5fb79292b
Esteban Echeverry <eecheverry@nubark.com>
2018-09-22 20:43:36 -0500

    Definir resolvers para simplificar la inicialización de los composers

---
+++ facturark/resolver.py
@@ -0,0 +1,81 @@
+from facturark.composers import (
+    ItemComposer, PriceComposer, InvoiceLineComposer, PartyTaxSchemeComposer,
+    PartyLegalEntityComposer, PersonComposer, AddressComposer,
+    LocationComposer, PartyComposer, DespatchComposer, DeliveryComposer,
+    CustomerPartyComposer, SupplierPartyComposer, TaxSubtotalComposer,
+    TaxTotalComposer, PaymentComposer, AllowanceChargeComposer,
+    DeliveryTermsComposer, MonetaryTotalComposer, InvoiceComposer)
+
+
+def resolve_invoice_line_composer():
+    item_composer = ItemComposer()
+    price_composer = PriceComposer()
+    return InvoiceLineComposer(item_composer, price_composer)
+
+
+def resolve_party_composer():
+    party_tax_scheme_composer = PartyTaxSchemeComposer()
+    party_legal_entity_composer = PartyLegalEntityComposer()
+    person_composer = PersonComposer()
+    address_composer = AddressComposer()
+    location_composer = LocationComposer(address_composer)
+    return PartyComposer(
+        party_tax_scheme_composer, party_legal_entity_composer,
+        person_composer, location_composer)
+
+
+def resolve_despatch_composer():
+    party_tax_scheme_composer = PartyTaxSchemeComposer()
+    party_legal_entity_composer = PartyLegalEntityComposer()
+    person_composer = PersonComposer()
+    address_composer = AddressComposer()
+    location_composer = LocationComposer(address_composer)
+    party_composer = PartyComposer(
+        party_tax_scheme_composer, party_legal_entity_composer,
+        person_composer, location_composer)
+    return DespatchComposer(address_composer, party_composer)
+
+
+def resolve_delivery_composer():
+    party_tax_scheme_composer = PartyTaxSchemeComposer()
+    party_legal_entity_composer = PartyLegalEntityComposer()
+    person_composer = PersonComposer()
+    address_composer = AddressComposer()
+    location_composer = LocationComposer(address_composer)
+    party_composer = PartyComposer(
+        party_tax_scheme_composer, party_legal_entity_composer,
+        person_composer, location_composer)
+    despatch_composer = DespatchComposer(address_composer, party_composer)
+    return DeliveryComposer(address_composer, location_composer,
+                            party_composer, despatch_composer)
+
+
+def resolve_customer_party_composer():
+    party_composer = resolve_party_composer()
+    return CustomerPartyComposer(party_composer)
+
+
+def resolve_supplier_party_composer():
+    party_composer = resolve_party_composer()
+    return SupplierPartyComposer(party_composer)
+
+
+def resolve_tax_total_composer():
+    tax_subtotal_composer = TaxSubtotalComposer()
+    return TaxTotalComposer(tax_subtotal_composer)
+
+
+def resolve_invoice_composer():
+    invoice_line_composer = resolve_invoice_line_composer()
+    monetary_total_composer = MonetaryTotalComposer()
+    customer_party_composer = resolve_customer_party_composer()
+    supplier_party_composer = resolve_supplier_party_composer()
+    payment_composer = PaymentComposer()
+    tax_total_composer = resolve_tax_total_composer()
+    delivery_composer = resolve_delivery_composer()
+    allowance_charge_composer = AllowanceChargeComposer()
+    delivery_terms_composer = DeliveryTermsComposer()
+    return InvoiceComposer(
+        supplier_party_composer, customer_party_composer, delivery_composer,
+        delivery_terms_composer, payment_composer, allowance_charge_composer,
+        tax_total_composer, monetary_total_composer, invoice_line_composer)
---
+++ tests/test_resolver.py
@@ -0,0 +1,41 @@
+from facturark.composers import (
+    InvoiceLineComposer, PartyComposer, DespatchComposer, DeliveryComposer,
+    CustomerPartyComposer, SupplierPartyComposer, TaxTotalComposer,
+    InvoiceComposer)
+from facturark.resolver import (
+    resolve_invoice_line_composer, resolve_party_composer,
+    resolve_despatch_composer, resolve_delivery_composer,
+    resolve_customer_party_composer, resolve_supplier_party_composer,
+    resolve_tax_total_composer, resolve_invoice_composer)
+
+
+def test_resolve_invoice_line_composer():
+    assert isinstance(resolve_invoice_line_composer(), InvoiceLineComposer)
+
+
+def test_resolve_party_composer():
+    assert isinstance(resolve_party_composer(), PartyComposer)
+
+
+def test_resolve_despatch_composer():
+    assert isinstance(resolve_despatch_composer(), DespatchComposer)
+
+
+def test_resolve_delivery_composer():
+    assert isinstance(resolve_delivery_composer(), DeliveryComposer)
+
+
+def test_resolve_customer_party_composer():
+    assert isinstance(resolve_customer_party_composer(), CustomerPartyComposer)
+
+
+def test_resolve_supplier_party_composer():
+    assert isinstance(resolve_supplier_party_composer(), SupplierPartyComposer)
+
+
+def test_resolve_tax_total_composer():
+    assert isinstance(resolve_tax_total_composer(), TaxTotalComposer)
+
+
+def test_resolve_invoice_composer():
+    assert isinstance(resolve_invoice_composer(), InvoiceComposer)
