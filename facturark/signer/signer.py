0e554d25f1f460661a2eda5da18d629eaa9c5fe6
Esteban Echeverry <eecheverry@nubark.com>
2018-09-27 16:37:44 -0500

    Setup sign method in signer

--- facturark/signer/signer.py
+++ facturark/signer/signer.py
@@ -3,3 +3,13 @@
 class Signer:
     def __init__(self, canonicalizer):
         self.canonicalizer = canonicalizer
+
+    def sign(self, element, pkcs12_certificate, pkcs12_password):
+
+        # Canonicalize Document
+        document = self.canonicalizer.canonicalize(element)
+
+        # Digest Complete Document
+        # self.
+
+        return True
--- tests/signer/test_signer.py
+++ tests/signer/test_signer.py
@@ -1,13 +1,41 @@
+import os
+import io
 from pytest import fixture
+from lxml.etree import parse
 from facturark.signer import Signer, Canonicalizer
 
 
 @fixture
 def signer():
-    canonicalizer = Canonicalizer
+    canonicalizer = Canonicalizer()
     signer = Signer(canonicalizer)
     return signer
 
 
+@fixture
+def unsigned_invoice():
+    filename = 'unsigned_invoice.xml'
+    directory = os.path.dirname(os.path.realpath(__file__))
+    element = parse(os.path.join(directory, '..', 'data', filename))
+    return element
+
+
+@fixture
+def pkcs12_certificate():
+    filename = 'certificate.p12'
+    directory = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(directory, '..', 'data', filename)
+    with io.open(path, 'rb') as f:
+        certificate = f.read()
+    password = 'test'
+    return (certificate, password)
+
+
 def test_signer_instantiation(signer):
     assert signer is not None
+
+
+def test_signer_sign(signer, unsigned_invoice, pkcs12_certificate):
+    certificate, password = pkcs12_certificate
+    result = signer.sign(unsigned_invoice, certificate, password)
+    assert result is True
