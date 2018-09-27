32ffe681f99eb64ba13cb33bee8aca68b8d5e6a6
Esteban Echeverry <eecheverry@nubark.com>
2018-09-27 08:52:12 -0500

    Change test_encoder comparison to binary

--- tests/signer/test_encoder.py
+++ tests/signer/test_encoder.py
@@ -15,4 +15,4 @@ def test_encoder_instantiation(encoder):
 def test_encoder_base64_encode(encoder):
     data = b'Binary'
     result = encoder.base64_encode(data)
-    assert result == 'QmluYXJ5'
+    assert result == b'QmluYXJ5'
