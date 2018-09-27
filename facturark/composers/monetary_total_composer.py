9e4bd319446a41d2186d933ade9ef7a272d5cf7a
Esteban Echeverry <eecheverry@nubark.com>
2018-09-27 08:32:51 -0500

    Move make_child to the facturark.utils package

--- facturark/composers/monetary_total_composer.py
+++ facturark/composers/monetary_total_composer.py
@@ -1,7 +1,7 @@
 from lxml.etree import Element, QName
+from facturark.utils import make_child
 from .namespaces import NS
 from .composer import Composer
-from .utils import make_child
 
 
 class MonetaryTotalComposer(Composer):
