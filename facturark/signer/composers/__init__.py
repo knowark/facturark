c83ff8605dae02566cb57d8929aeb32b775dd423
Esteban Echeverry <eecheverry@nubark.com>
2018-09-27 19:48:30 -0500

    Create object element, signed properties and qualyfing properties

--- facturark/signer/composers/__init__.py
+++ facturark/signer/composers/__init__.py
@@ -1,5 +1,6 @@
 from .namespaces import NS
 from .key_info_composer import KeyInfoComposer
+from .object_composer import ObjectComposer
 from .reference_composer import ReferenceComposer
 from .signature_composer import SignatureComposer
 from .signed_info_composer import SignedInfoComposer
---
+++ facturark/signer/composers/object_composer.py
@@ -0,0 +1,13 @@
+from lxml.etree import Element, SubElement, QName, tostring
+from facturark.utils import make_child
+from .namespaces import NS
+from .composer import Composer
+
+
+class ObjectComposer(Composer):
+
+    def compose(self, data_dict, root_name=None):
+        root_name = root_name or self.root_name
+        root = Element(QName(NS.ds, root_name), nsmap=vars(NS))
+
+        return root
