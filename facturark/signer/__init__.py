8f63127d7a18881525ea458248b7eeb0ba7883d8
Esteban Echeverry <eecheverry@nubark.com>
2018-09-29 12:54:27 -0500

    Create Identifier component

--- facturark/signer/__init__.py
+++ facturark/signer/__init__.py
@@ -2,5 +2,6 @@ from .canonicalizer import Canonicalizer
 from .encoder import Encoder
 from .encrypter import Encrypter
 from .hasher import Hasher
+from .identifier import Identifier
 from .signer import Signer
 from .verifier import Verifier
