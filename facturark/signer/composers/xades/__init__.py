a33ad293ff764928e7b8870c500187e096d7ee9e
eecheverry <eecheverry@nubark.com>
2018-10-11 15:10:53 -0500

    Add xades signer role composer

--- facturark/signer/composers/xades/__init__.py
+++ facturark/signer/composers/xades/__init__.py
@@ -2,3 +2,4 @@ from .qualifying_properties_composer import QualifyingPropertiesComposer
 from .signed_properties_composer import SignedPropertiesComposer
 from .signed_signature_properties_composer import (
     SignedSignaturePropertiesComposer)
+from .signer_role_composer import SignerRoleComposer
---
+++ tests/signer/composers/xades/test_signer_role_composer.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from pytest import fixture
+from lxml.etree import QName, fromstring, tostring
+from facturark.signer.composers import NS
+from facturark.signer.composers.xades import SignerRoleComposer
+
+
+@fixture
+def composer():
+    return SignerRoleComposer()
+
+
+@fixture
+def data_dict():
+    return {
+        'claimed_roles': [{
+            'claimed_role': 'supplier'
+        }]
+    }
+
+
+def test_compose(composer, data_dict, schema):
+    signer_role = composer.compose(data_dict)
+
+    assert signer_role.prefix == "xades"
+    assert signer_role.tag == QName(
+        NS.xades, "SignerRole").text
+    assert signer_role.findtext(
+        'xades:ClaimedRoles/xades:ClaimedRole',
+        namespaces=vars(NS)) == 'supplier'
+
+    schema.assertValid(signer_role)
