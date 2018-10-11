1fbf9a66041a2f61dac398ce2703f4ef20fc7189
Esteban Echeverry <eecheverry@nubark.com>
2018-10-11 13:40:00 -0500

    Add DIAN extensions to invoice

--- tests/composers/test_invoice_composers.py
+++ tests/composers/test_invoice_composers.py
@@ -14,11 +14,36 @@ def composer():
 @fixture
 def data_dict():
     return {
-        'extensions': [
-            {
-                'extension_content': {}
+        'extensions': [{
+            'extension_content': {
+                'dian_extensions': {
+                    'invoice_control': {
+                        'invoice_authorization': '9000000500017960',
+                        'authorization_period': {
+                            'start_date': '2016-07-11',
+                            'end_date': '2016-07-11'
+                        },
+                        'authorized_invoices': {
+                            'prefix': 'PRUE',
+                            'from': '980000000',
+                            'to': '985000000'
+                        }
+                    },
+                    'invoice_source': {
+                        'identification_code': 'CO'
+                    },
+                    'software_provider': {
+                        'provider_id': '900373115',
+                        'software_id': '0d2e2883-eb8d-4237-87fe-28aeb71e961e'
+                    },
+                    'software_security_code': (
+                        "bdaa51c9953e08dcc8f398961f7cd0717cd5fbea356e93766"
+                        "0aa1a8abbe31f4c9b4eb5cf8682eaca4c8523953253dcce")
+                }
             }
-        ],
+        }, {
+            'extension_content': {}
+        }],
         "id": "F0001",
         "uuid": "a3d6c86a71cbc066aaa19fd363c0fe4b5778d4a0",
         "issue_date": "2018-09-13",
