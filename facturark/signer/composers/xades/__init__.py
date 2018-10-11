352ae57035891945fa8be462dbea1c145f7bafaf
eecheverry <eecheverry@nubark.com>
2018-10-11 16:04:36 -0500

    Create signature policy identifier composer

--- facturark/signer/composers/xades/__init__.py
+++ facturark/signer/composers/xades/__init__.py
@@ -3,3 +3,5 @@ from .signed_properties_composer import SignedPropertiesComposer
 from .signed_signature_properties_composer import (
     SignedSignaturePropertiesComposer)
 from .signer_role_composer import SignerRoleComposer
+from .signature_policy_identifier_composer import (
+    SignaturePolicyIdentifierComposer)
