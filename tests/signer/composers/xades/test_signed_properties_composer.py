673e8f9210221184b9695b51a75e6343558b6547
eecheverry <eecheverry@nubark.com>
2018-10-12 10:20:19 -0500

    Include signed_properties in signer process

--- tests/signer/composers/xades/test_signed_properties_composer.py
+++ tests/signer/composers/xades/test_signed_properties_composer.py
@@ -2,19 +2,65 @@
 from pytest import fixture
 from lxml.etree import QName, fromstring, tostring
 from facturark.signer.composers import NS
-from facturark.signer.composers.xades import SignedPropertiesComposer
+from facturark.signer.resolver import resolve_signed_properties_composer
 
 
 @fixture
 def composer():
-    return SignedPropertiesComposer()
+    return resolve_signed_properties_composer()
 
 
 @fixture
 def data_dict():
     return {
         '@attributes': {'Id': ("xmldsig-88fbfc45-3be2-"
-                               "4c4a-83ac-0796e1bad4c5-signedprops")}
+                               "4c4a-83ac-0796e1bad4c5-signedprops")},
+        'signed_signature_properties': {
+            'signing_time': '2016-07-12T11:17:38.639-05:00',
+            'signing_certificate': {
+                'certs': [{
+                    'cert_digest': {
+                        'digest_method': {
+                            '@attributes': {
+                                'Algorithm': (
+                                    "http://www.w3.org/2000/09/xmldsig#sha1")}
+                        },
+                        'digest_value': '2el6MfWvYsvEaa/TV513a7tVK0g='
+                    },
+                    'issuer_serial': {
+                        'X509_issuer_name': (
+                            'C=CO,L=Bogota D.C.,O=Andes SCD.,OU=Division '
+                            'de certificacion entidad final,CN=CA ANDES SCD '
+                            'S.A. Clase II,1.2.840.113549.1.9.1=#1614696e666f'
+                            '40616e6465737363642e636f6d2e636f'),
+                        'X509_serial_number': '9128602840918470673'
+                    }
+                }]
+            },
+            'signature_policy_identifier': {
+                'signature_policy_id': {
+                    'sig_policy_id': {
+                        'identifier': (
+                            'https://facturaelectronica.dian.gov.co/'
+                            'politicadefirma/v1/politicadefirmav1.pdf')
+                    },
+                    'sig_policy_hash': {
+                        'digest_method': {
+                            '@attributes': {
+                                'Algorithm': (
+                                    "http://www.w3.org/2000/09/xmldsig#sha1")
+                            }
+                        },
+                        'digest_value': '61fInBICBQOCBwuTwlaOZSi9HKc='
+                    }
+                }
+            },
+            'signer_role': {
+                'claimed_roles': [{
+                    'claimed_role': 'supplier'
+                }]
+            }
+        }
     }
 
 
@@ -26,5 +72,8 @@ def test_compose(composer, data_dict, schema):
         NS.xades, "SignedProperties").text
     assert signed_properties.attrib.get('Id') == (
         data_dict['@attributes']['Id'])
+
+    assert signed_properties.find(
+        QName(NS.xades, 'SignedSignatureProperties')) is not None
 
     schema.assertValid(signed_properties)
--- tests/signer/composers/xades/test_signed_signature_properties_composer.py
+++ tests/signer/composers/xades/test_signed_signature_properties_composer.py
@@ -2,19 +2,69 @@
 from pytest import fixture
 from lxml.etree import QName, fromstring, tostring
 from facturark.signer.composers import NS
-from facturark.signer.composers.xades import SignedSignaturePropertiesComposer
+from facturark.signer.composers.xades import (
+    SignedSignaturePropertiesComposer, SignaturePolicyIdentifierComposer,
+    SignerRoleComposer, SigningCertificateComposer)
 
 
 @fixture
 def composer():
-    return SignedSignaturePropertiesComposer()
+    signing_certificate_composer = SigningCertificateComposer()
+    signature_policy_identifier_composer = (
+        SignaturePolicyIdentifierComposer())
+    signer_role_composer = SignerRoleComposer()
+    return SignedSignaturePropertiesComposer(
+        signing_certificate_composer,
+        signature_policy_identifier_composer,
+        signer_role_composer)
 
 
 @fixture
 def data_dict():
     return {
-        'signing_time': '2016-07-12T11:17:38.639-05:00'
-
+        'signing_time': '2016-07-12T11:17:38.639-05:00',
+        'signing_certificate': {
+            'certs': [{
+                'cert_digest': {
+                    'digest_method': {
+                        '@attributes': {
+                            'Algorithm': (
+                                "http://www.w3.org/2000/09/xmldsig#sha1")}
+                    },
+                    'digest_value': '2el6MfWvYsvEaa/TV513a7tVK0g='
+                },
+                'issuer_serial': {
+                    'X509_issuer_name': (
+                        'C=CO,L=Bogota D.C.,O=Andes SCD.,OU=Division '
+                        'de certificacion entidad final,CN=CA ANDES SCD '
+                        'S.A. Clase II,1.2.840.113549.1.9.1=#1614696e666f'
+                        '40616e6465737363642e636f6d2e636f'),
+                    'X509_serial_number': '9128602840918470673'
+                }
+            }]
+        },
+        'signature_policy_identifier': {
+            'signature_policy_id': {
+                'sig_policy_id': {
+                    'identifier': ('https://facturaelectronica.dian.gov.co/'
+                                   'politicadefirma/v1/politicadefirmav1.pdf')
+                },
+                'sig_policy_hash': {
+                    'digest_method': {
+                        '@attributes': {
+                            'Algorithm': (
+                                "http://www.w3.org/2000/09/xmldsig#sha1")
+                        }
+                    },
+                    'digest_value': '61fInBICBQOCBwuTwlaOZSi9HKc='
+                }
+            }
+        },
+        'signer_role': {
+            'claimed_roles': [{
+                'claimed_role': 'supplier'
+            }]
+        }
     }
 
 
