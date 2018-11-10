7cf30c9428d63258681629700d25f793fa9ed123
Esteban Echeverry <eecheverry@nubark.com>
2018-11-10 12:26:39 -0500

    Bump version to 0.3.0

--- facturark/__init__.py
+++ facturark/__init__.py
@@ -6,4 +6,4 @@ from .api import build_invoice, send_invoice, verify_document, query_document
 
 __author__ = """Nubark"""
 __email__ = 'info@nubark.com'
-__version__ = '0.2.0'
+__version__ = '0.3.0'
