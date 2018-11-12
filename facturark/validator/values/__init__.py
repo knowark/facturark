283982e8bc2df2d2531963b96871f1d237f32eac
Esteban Echeverry <eecheverry@nubark.com>
2018-11-12 10:59:36 -0500

    Include countries and currencies in value lists

--- facturark/validator/values/__init__.py
+++ facturark/validator/values/__init__.py
@@ -1,3 +1,5 @@
 from .v01_invoice_types import INVOICE_TYPES
 from .v02_identity_document_types import IDENTITY_DOCUMENT_TYPES
+from .v07_currencies import CURRENCIES
+from .v08_countries import COUNTRIES
 from .v09_party_types import PARTY_TYPES
