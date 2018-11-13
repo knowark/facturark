ac8b1d4154c7047b6b7d9d8ef0acc7e4ac157bd0
eecheverry <eecheverry@nubark.com>
2018-11-13 11:42:49 -0500

    Add credit and debit notes concepts

---
+++ facturark/validator/values/v03_credit_note_concepts.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# 8.1 Concepto de Notas crédito
+
+CREDIT_NOTE_CONCEPTS = {
+    "1": (u"Devolución de parte de los bienes; "
+          u"no aceptación de partes del servicio"),
+    "2": u"Anulación de factura electrónica",
+    "3": u"Rebaja total aplicada",
+    "4": u"Descuento total aplicado",
+    "5": u"Rescisión: nulidad por falta de requisito",
+    "6": u"Otros"
+}
---
+++ facturark/validator/values/v04_debit_note_concepts.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# 8.1 Concepto de Notas débito
+
+DEBIT_NOTE_CONCEPTS = {
+    "1": u"Intereses",
+    "2": u"Gastos por cobrar",
+    "3": u"Cambio del valor"
+}
