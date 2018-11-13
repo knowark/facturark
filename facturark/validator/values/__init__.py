19c27f0187a3bbc7b2367c5f2b31b8d23bf615ff
eecheverry <eecheverry@nubark.com>
2018-11-13 13:57:23 -0500

    Add payment methods

--- facturark/validator/values/__init__.py
+++ facturark/validator/values/__init__.py
@@ -3,6 +3,7 @@ from .v02_identity_document_types import IDENTITY_DOCUMENT_TYPES
 from .v03_credit_note_concepts import CREDIT_NOTE_CONCEPTS
 from .v04_debit_note_concepts import DEBIT_NOTE_CONCEPTS
 from .v05_tax_types import TAX_TYPES
+from .v06_payment_methods import PAYMENT_METHODS
 from .v07_currencies import CURRENCIES
 from .v08_countries import COUNTRIES
 from .v09_party_types import PARTY_TYPES
---
+++ facturark/validator/values/v06_payment_methods.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# 8.6 Medios de pago
+
+PAYMENT_METHODS = {
+    "10": u"Efectivo",
+    "20": u"Cheque",
+    "41": u"Transferencia bancaria",
+    "42": u"Consignación bancaria"
+}
