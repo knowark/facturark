4ce67c5144100b5bd53311d1ef474bad206ad124
eecheverry <eecheverry@nubark.com>
2018-11-13 10:00:30 -0500

    Include reviews in validation process

--- facturark/validator/validator.py
+++ facturark/validator/validator.py
@@ -3,11 +3,13 @@ from ..utils import parse_xsd
 
 class Validator:
 
-    def __init__(self, uuid_generator):
+    def __init__(self, uuid_generator, reviewer):
         self.schema = parse_xsd("XSD/DIAN/DIAN_UBL.xsd")
         self.uuid_generator = uuid_generator
+        self.reviewer = reviewer
 
     def validate(self, element):
         element, uuid = self.uuid_generator.generate(element)
         self.schema.assertValid(element)
+        self.reviewer.review(element)
         return element, uuid
