f206d14e42e2b03c4787c1683d38edbb5da4f21e
eecheverry <eecheverry@nubark.com>
2018-12-07 09:42:36 -0500

    Remove debugging statement in date plugin

--- facturark/client/date_plugin.py
+++ facturark/client/date_plugin.py
@@ -4,7 +4,4 @@ from zeep import Plugin
 
 class DatePlugin(Plugin):
     def egress(self, envelope, http_headers, operation, binding_options):
-        serialized_envelope = tostring(
-            envelope).replace(b'2018-11-20T23:21:04', b'2018-11-20T23:21:04')
-        envelope = fromstring(serialized_envelope)
         return envelope, http_headers
