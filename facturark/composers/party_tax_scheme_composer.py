ce42ded18c06a7a772228d3a7664008665e73541
David Arnold <dar@xoe.solutions>
2018-12-11 22:16:47 -0500

    WIP implement stubs for min req tax credit values

--- facturark/composers/party_tax_scheme_composer.py
+++ facturark/composers/party_tax_scheme_composer.py
@@ -13,6 +13,14 @@ class PartyTaxSchemeComposer(Composer):
         for tax_level_code in data_dict.get("tax_level_code"):
             make_child(root, QName(NS.cbc, 'TaxLevelCode'),
                        tax_level_code, required=False)
+
+        # TODO: Make them required
+        make_child(root, QName(NS.cac, 'RegistrationName'),
+                   required=True, empty=True)
+        make_child(root, QName(NS.cac, 'RegistrationAddress'),
+                   required=True, empty=True)
+        make_child(root, QName(NS.cac, 'CompanyID'),
+                   required=True, empty=True)
         make_child(root, QName(NS.cac, 'TaxScheme'),
                    required=False, empty=True)
 
