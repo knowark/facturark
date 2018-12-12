448a434316ac3189e41b82b4ce498ec443f09151
eecheverry <eecheverry@nubark.com>
2018-12-12 17:24:45 -0500

    Export value lists from values package

--- facturark/validator/values/__init__.py
+++ facturark/validator/values/__init__.py
@@ -8,3 +8,9 @@ from .v07_currencies import CURRENCIES
 from .v08_countries import COUNTRIES
 from .v09_party_types import PARTY_TYPES
 from .v10_tax_levels import TAX_LEVELS
+from .v11_incoterms import INCOTERMS
+from .v12_payment_types import PAYMENT_TYPES
+from .v13_representation_types import REPRESENTATION_TYPES
+from .v14_liability_types import LIABILITY_TYPES
+from .v15_customs_users_types import CUSTOMS_USERS_TYPES
+from .v16_establishment_types import ESTABLISHMENT_TYPES
